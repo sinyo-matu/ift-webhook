c22d349f14de6e3f9ba792a9c0262e6d6edbd8dc
sinyo-matu <seelerei0130@gmail.com>
2021-03-13 20:37:35 +0900

    update README

--- README.md
+++ README.md
@@ -12,32 +12,32 @@ and the non-blocking interface use [reqwest](https://crates.io/crates/reqwest) i
 about ifttt webhook usage:
 For example,You can call a url (supplied by ifttt) then receive a notification (could include data* you supplied) on you phone.
 
-*sometimes you can set some json data (up to three fields in it) on the request, which is depends on the service webhook connected with.
+*sometimes you can set some json data (up to three fields in it) inside the request, which is depends on the service webhook connected with.
 
 # Installation
 
-- find in [crates.io](https://crates.io/crates/ift-webhook)
+- find on [crates.io](https://crates.io/crates/ift-webhook)
 
 - use [cargo-edit](https://crates.io/crates/cargo-edit)
 ```sh
 cargo add ift-webhook
 ```
 
-# Usage
+# Hot to use
 ### blocking api
 *code*
 ```rust
-    extern crate ift-webhook
-    extern crate dotenv
-    use ift_webhook::*
+extern crate ift-webhook
+extern crate dotenv
+use ift_webhook::*
 
-    dotenv::dotenv().unwrap();
-    let event_name = dotenv::var("EVENT").unwrap();
-    let api_key = dotenv::var("KEY").unwrap();
-    let client = IftWHClient::new(&api_key);
-    let data = WebHookData::new(Some("test1"), Some("test2"), Some("test3"));
-    let res = client.trigger(&event_name, data);
-    assert!(res.is_ok())
+dotenv::dotenv().unwrap();
+let event_name = dotenv::var("EVENT").unwrap();
+let api_key = dotenv::var("KEY").unwrap();
+let client = IftWHClient::new(&api_key);
+let data = WebHookData::new(Some("test1"), Some("test2"), Some("test3"));
+let res = client.trigger(&event_name, data);
+assert!(res.is_ok())
 ```
 ### non-blocking api
 *Cargo.toml*
@@ -46,16 +46,16 @@ ift-webhook={version=*,default-features= false,features=["non-blocking"]}
 ```
 *code*
 ```rust
-    extern crate ift-webhook
-    extern crate dotenv
-    use ift_webhook::*
+extern crate ift-webhook
+extern crate dotenv
+use ift_webhook::*
 
-    dotenv::dotenv().unwrap();
-    let event_name = dotenv::var("EVENT").unwrap();
-    let api_key = dotenv::var("KEY").unwrap();
-    let client = AsyncIftWHClient::new(&api_key);
-    let res = client.trigger(&event_name, None).await;
-    assert!(res.is_ok())
+dotenv::dotenv().unwrap();
+let event_name = dotenv::var("EVENT").unwrap();
+let api_key = dotenv::var("KEY").unwrap();
+let client = AsyncIftWHClient::new(&api_key);
+let res = client.trigger(&event_name, None).await;
+assert!(res.is_ok())
 ```
 ### non-blocking api with time delay
 *Cargo.toml*
@@ -64,17 +64,17 @@ ift-webhook={version=*,default-features= false,features=["delay"]}
 ```
 *code*
 ```rust
-    extern crate ift-webhook
-    extern crate dotenv
-    use ift_webhook::*
-    
-    dotenv::dotenv().unwrap();
-    let event_name = dotenv::var("EVENT").unwrap();
-    let api_key = dotenv::var("KEY").unwrap();
-    let client = AsyncIftWHClient::new(&api_key);
-    let res_handler: DelayResultHandler =
-        client.trigger_with_delay(&event_name, None, std::time::Duration::from_secs(5));
-    ///do something else
-    let res = res_handler.await;
-    assert!(res.is_ok())
+extern crate ift-webhook
+extern crate dotenv
+use ift_webhook::*
+
+dotenv::dotenv().unwrap();
+let event_name = dotenv::var("EVENT").unwrap();
+let api_key = dotenv::var("KEY").unwrap();
+let client = AsyncIftWHClient::new(&api_key);
+let res_handler: DelayResultHandler =
+    client.trigger_with_delay(&event_name, None, std::time::Duration::from_secs(5));
+///do something else
+let res = res_handler.await;
+assert!(res.is_ok())
 ```
--- src/test.rs
+++ src/test.rs
@@ -1,61 +1,59 @@
 #[cfg(test)]
-mod tests {
-    use crate::*;
+use crate::*;
 
-    #[tokio::test]
-    async fn non_blocking_trigger_without_data() {
-        dotenv::dotenv().unwrap();
-        let event_name = dotenv::var("EVENT").unwrap();
-        let api_key = dotenv::var("KEY").unwrap();
-        let client = AsyncIftWHClient::new(&api_key);
-        let res = client.trigger(&event_name, None).await;
+#[tokio::test]
+async fn non_blocking_trigger_without_data() {
+    dotenv::dotenv().unwrap();
+    let event_name = dotenv::var("EVENT").unwrap();
+    let api_key = dotenv::var("KEY").unwrap();
+    let client = AsyncIftWHClient::new(&api_key);
+    let res = client.trigger(&event_name, None).await;
 
-        assert!(res.is_ok())
-    }
+    assert!(res.is_ok())
+}
 
-    #[tokio::test]
-    async fn non_blocking_trigger_with_data() {
-        dotenv::dotenv().unwrap();
-        let event_name = dotenv::var("EVENT").unwrap();
-        let api_key = dotenv::var("KEY").unwrap();
-        let data = WebHookData::new(Some("test_blocking1"), Some("test2"), None);
-        let client = AsyncIftWHClient::new(&api_key);
-        let res = client.trigger(&event_name, data).await;
+#[tokio::test]
+async fn non_blocking_trigger_with_data() {
+    dotenv::dotenv().unwrap();
+    let event_name = dotenv::var("EVENT").unwrap();
+    let api_key = dotenv::var("KEY").unwrap();
+    let data = WebHookData::new(Some("test_blocking1"), Some("test2"), None);
+    let client = AsyncIftWHClient::new(&api_key);
+    let res = client.trigger(&event_name, data).await;
 
-        assert!(res.is_ok())
-    }
+    assert!(res.is_ok())
+}
 
-    #[tokio::test]
-    async fn delay_trigger_without_data() {
-        dotenv::dotenv().unwrap();
-        let event_name = dotenv::var("EVENT").unwrap();
-        let api_key = dotenv::var("KEY").unwrap();
-        let client = AsyncIftWHClient::new(&api_key);
-        let res_handler: DelayResultHandler =
-            client.trigger_with_delay(&event_name, None, std::time::Duration::from_secs(5));
-        println!("yo");
-        let res = res_handler.await;
-        assert!(res.is_ok())
-    }
+#[tokio::test]
+async fn delay_trigger_without_data() {
+    dotenv::dotenv().unwrap();
+    let event_name = dotenv::var("EVENT").unwrap();
+    let api_key = dotenv::var("KEY").unwrap();
+    let client = AsyncIftWHClient::new(&api_key);
+    let res_handler: DelayResultHandler =
+        client.trigger_with_delay(&event_name, None, std::time::Duration::from_secs(5));
+    println!("yo");
+    let res = res_handler.await;
+    assert!(res.is_ok())
+}
 
-    #[test]
-    fn blocking_trigger_without_data() {
-        dotenv::dotenv().unwrap();
-        let event_name = dotenv::var("EVENT").unwrap();
-        let api_key = dotenv::var("KEY").unwrap();
-        let client = IftWHClient::new(&api_key);
-        let res = client.trigger(&event_name, None);
-        assert!(res.is_ok())
-    }
+#[test]
+fn blocking_trigger_without_data() {
+    dotenv::dotenv().unwrap();
+    let event_name = dotenv::var("EVENT").unwrap();
+    let api_key = dotenv::var("KEY").unwrap();
+    let client = IftWHClient::new(&api_key);
+    let res = client.trigger(&event_name, None);
+    assert!(res.is_ok())
+}
 
-    #[test]
-    fn blocking_trigger_with_data() {
-        dotenv::dotenv().unwrap();
-        let event_name = dotenv::var("EVENT").unwrap();
-        let api_key = dotenv::var("KEY").unwrap();
-        let client = IftWHClient::new(&api_key);
-        let data = WebHookData::new(Some("test1"), Some("test2"), Some("test3"));
-        let res = client.trigger(&event_name, data);
-        assert!(res.is_ok())
-    }
+#[test]
+fn blocking_trigger_with_data() {
+    dotenv::dotenv().unwrap();
+    let event_name = dotenv::var("EVENT").unwrap();
+    let api_key = dotenv::var("KEY").unwrap();
+    let client = IftWHClient::new(&api_key);
+    let data = WebHookData::new(Some("test1"), Some("test2"), Some("test3"));
+    let res = client.trigger(&event_name, data);
+    assert!(res.is_ok())
 }
